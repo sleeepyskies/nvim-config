7878a7e3078726b21ca852cc970593969e1d4915
sleeepyskies <skyswalla@gmail.com>
2025-05-13 08:11:27 +0200

    new formatter lol

--- lua/watermelon/core/options.lua
+++ lua/watermelon/core/options.lua
@@ -1,5 +1,5 @@
 -- stylua: ignore
--- This file is used to customise and set options in vim (done using vim.opt.xxx)
+-- This file is used to customize and set options in vim (done using vim.opt.xxx)
 
 -- newtr: use tree style directory listings.
 vim.cmd('let g:netrw_liststyle = 3')
@@ -11,9 +11,9 @@ opt.relativenumber = true
 opt.number = true
 
 -- tabs and indents
-opt.tabstop = 4       -- use 4 spaces for tabs
-opt.shiftwidth = 4    -- set number of spaces to use for indenting using > in vim
-opt.expandtab = true  -- convert tabs to spaces
+opt.tabstop = 4 -- use 4 spaces for tabs
+opt.shiftwidth = 4 -- set number of spaces to use for indenting using > in vim
+opt.expandtab = true -- convert tabs to spaces
 opt.autoindent = true -- copy previous line indentation when starting a newline
 
 -- line wrapping
@@ -21,7 +21,7 @@ opt.wrap = false -- disable line wrapping
 
 -- search
 opt.ignorecase = true -- case insensitive searches
-opt.smartcase = true  -- using mixed case enforces case sensitivity
+opt.smartcase = true -- using mixed case enforces case sensitivity
 
 -- cursor line
 opt.cursorline = true -- highlight line cursor is on
@@ -31,8 +31,8 @@ opt.colorcolumn = '80,100,120' -- provide line guards
 
 -- appearance
 opt.termguicolors = true -- enable true color in terminal
-opt.background = 'dark'  -- prefer dark themes
-opt.signcolumn = 'yes'   -- enable a column that can be used for icons
+opt.background = 'dark' -- prefer dark themes
+opt.signcolumn = 'yes' -- enable a column that can be used for icons
 
 -- clipboard
 opt.clipboard:append('unnamedplus') -- use system clipboard for yanking!
