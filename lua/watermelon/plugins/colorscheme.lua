13992cbae32455f47acddc5ba3cb275a2bb6d285
sleeepyskies <skyswalla@gmail.com>
2025-05-17 12:16:10 +0200

    Add new colorscheme and configure, old one still remains commented

--- lua/watermelon/plugins/colorscheme.lua
+++ lua/watermelon/plugins/colorscheme.lua
@@ -1,58 +1,78 @@
--- material deep ocean theme
+-- onedark dark theme
 return {
-    'marko-cerovac/material.nvim',
-    priority = 1000,
+    'olimorris/onedarkpro.nvim',
+    priority = 1000, -- Ensure it loads first
     config = function()
-        vim.g.material_style = 'deep ocean'
-
-        require('material').setup({
+        -- somewhere in your config:
+        vim.cmd('colorscheme onedark_dark')
+        require('onedarkpro').setup({
             styles = {
-                comments = { italic = true },
-                strings = { bold = true },
-                keywords = { italic = true },
-                functions = { bold = true },
-                variables = {},
-                operators = {},
-                types = {},
+                comments = 'italic',
+                keywords = 'italic',
             },
-            -- enable transparent background
-            disable = {
-                background = true,
-            },
-            plugins = { -- Uncomment the plugins that you use to highlight them
-                -- Available plugins:
-                -- 'coc',
-                -- 'colorful-winsep',
-                -- 'dap',
-                -- 'dashboard',
-                -- 'eyeliner',
-                -- 'fidget',
-                -- 'flash',
-                'gitsigns',
-                -- 'harpoon',
-                -- 'hop',
-                -- 'illuminate',
-                'indent-blankline',
-                -- 'lspsaga',
-                -- 'mini',
-                -- 'neogit',
-                -- 'neotest',
-                -- 'neo-tree',
-                -- 'neorg',
-                -- 'noice',
-                'nvim-cmp',
-                -- 'nvim-navic',
-                'nvim-tree',
-                'nvim-web-devicons',
-                -- 'rainbow-delimiters',
-                -- 'sneak',
-                'telescope',
-                -- 'trouble',
-                'which-key',
-                -- 'nvim-notify',
+            options = {
+                -- transparency = true,
+                cursorline = true,
             },
         })
-
-        vim.cmd('colorscheme material')
     end,
 }
+
+-- material deep ocean theme
+-- return {
+--     'marko-cerovac/material.nvim',
+--     priority = 1000,
+--     config = function()
+--         vim.g.material_style = 'deep ocean'
+--
+--         require('material').setup({
+--             styles = {
+--                 comments = { italic = true },
+--                 strings = { bold = true },
+--                 keywords = { italic = true },
+--                 functions = { bold = true },
+--                 variables = {},
+--                 operators = {},
+--                 types = {},
+--             },
+--             -- enable transparent background
+--             disable = {
+--                 background = true,
+--             },
+--             plugins = { -- Uncomment the plugins that you use to highlight them
+--                 -- Available plugins:
+--                 -- 'coc',
+--                 -- 'colorful-winsep',
+--                 -- 'dap',
+--                 -- 'dashboard',
+--                 -- 'eyeliner',
+--                 -- 'fidget',
+--                 -- 'flash',
+--                 'gitsigns',
+--                 -- 'harpoon',
+--                 -- 'hop',
+--                 -- 'illuminate',
+--                 'indent-blankline',
+--                 -- 'lspsaga',
+--                 -- 'mini',
+--                 -- 'neogit',
+--                 -- 'neotest',
+--                 -- 'neo-tree',
+--                 -- 'neorg',
+--                 -- 'noice',
+--                 'nvim-cmp',
+--                 -- 'nvim-navic',
+--                 'nvim-tree',
+--                 'nvim-web-devicons',
+--                 -- 'rainbow-delimiters',
+--                 -- 'sneak',
+--                 'telescope',
+--                 'trouble',
+--                 'which-key',
+--                 -- 'nvim-notify',
+--             },
+--         })
+--
+--         vim.cmd('colorscheme material')
+--     end,
+-- }
